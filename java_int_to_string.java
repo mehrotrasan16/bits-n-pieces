8b04117a837b6bb282ab230309c5c28b2abe9b60
Sanket Mehrotra <sanketmehrotra102@gmail.com>
2019-11-24 12:28:18 -0700

    Create java_int_to_string.java

---
+++ java_int_to_string.java
@@ -0,0 +1,57 @@
+//Problem: https://www.hackerrank.com/challenges/java-int-to-string/problem
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+//SanketM start
+    //Method #1
+    //String s = String.valueOf(n);
+    
+    //Method #2
+    //String s = Integer.toString(n);
+    
+    //Method #3
+    Integer num = new Integer(n);
+    String s = num.toString();
+//SanketM end
+ 
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
