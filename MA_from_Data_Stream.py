4278eadf98d9c7e0aef23a02b0865b819da0edd8
Sanket Mehrotra <sanketmehrotra102@gmail.com>
2021-01-28 21:03:55 -0700

    Update MA_from_Data_Stream.py

--- MA_from_Data_Stream.py
+++ MA_from_Data_Stream.py
@@ -2,17 +2,25 @@
 #https://nifannn.github.io/2018/10/01/Algorithm-Notes-Leetcode-346-Moving-Average-from-Data-Stream/
 class MAFinder:
 
-    def __init__(self):
+    def __init__(self,size):
         """
         initialize your data structure here.
         """
-        pass
+        mylist = []
+        mysize = size
+        
 
     def addNum(self, num: int) -> None:
-        pass    
-
+        
+        if len(mylist) < size:
+            self.mylist.append(num)
+        else:
+            self.mylist.pop(0)
+            self.mylist.append(num)
+            
+            
     def findMA(self) -> float:
-        pass
+        return sum(self.mylist)/self.mysize
 
 # Your MedianFinder object will be instantiated and called as such:
 obj = MAFinder()
